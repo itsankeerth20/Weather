18bd8f4347fa29a8677904361bc2fd47d48d5057
itsankeerth <itsankeerth20@gmail.com>
2018-10-11 18:39:52 -0400

    First Commit

---
+++ Weather/Weather/Core.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    public class Core
+    {
+        public static async Task<WeatherModel> GetWeather(string zipCode)
+        {
+            //Sign up for a free API key at http://openweathermap.org/appid  
+            string key = "2c526a0142a73d3f29394eccf6ec1cb0";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+                + zipCode + ",us&appid=" + key + "&units=imperial";
+
+            dynamic results = await DataService.GetDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                WeatherModel weather = new WeatherModel();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Weather/Weather/WeatherMainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+namespace Weather
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WeatherMainPage : ContentPage
+	{
+		public WeatherMainPage ()
+		{
+			InitializeComponent ();
+            BindingContext = new WeatherModel();
+            //WeatherModel wm = new WeatherModel();
+            //BindingContext = wm;
+        }
+
+        private async void getWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                WeatherModel weather = await Core.GetWeather(zipCodeEntry.Text);
+                BindingContext = weather;
+                getWeatherBtn.Text = "Search Again";
+            }
+
+        }
+    }
+}
